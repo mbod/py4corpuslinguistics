913e03ab07411293c5c553978d711d4221a58ead
Matt O'Donnell <matt@opentext.org>
2011-07-11 16:23:01 -0400

    Birmingham workshop

---
+++ workshops/Birmingham_19July2011/scripts/task1_skeleton.py
@@ -0,0 +1,32 @@
+
+
+# 1. load contents of a file into a string value named text
+
+
+# 2.split the value into a list of tokens using whitespace as delimitation
+
+
+# 3. apply normalization to tokens in list to:
+#    a. transform to lower case 
+#    b. remove trailing punctuation
+
+
+# 4. count the total number of types and tokens and calculate type-token ratio
+
+
+# 5. count frequency of selected words
+
+
+# 6. create a frequency distribution (all types and their token counts)
+
+
+# 7. sort and display a frequency list
+
+
+# 8. write frequency list out to a file
+
+
+# 9. create a simple index recording word positions
+
+
+# 10. search for a word and produce a simple KWIC display
