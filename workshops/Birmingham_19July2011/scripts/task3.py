760171f0c27e2890a0becb51d2a4d209036f19aa
Matt O'Donnell <matt@opentext.org>
2011-07-15 00:27:22 -0400

    Matt - task3 update

--- workshops/Birmingham_19July2011/scripts/task3.py
+++ workshops/Birmingham_19July2011/scripts/task3.py
@@ -25,18 +25,19 @@ for file_pos,file in enumerate(in_files):
 
 	text = open(os.path.join(in_directory,file)).read()
 
-	# 2.split the value into a list of tokens using whitespace as delimitation
-
+	# 5. split the value into a list of tokens using whitespace as delimitation
 	forms = text.split()
+
+	# 6. add form list to text_list
 	text_list.append(forms)
 
-	# 3. apply normalization to tokens in list to:
+	# 7. apply normalization to tokens in list to:
 	#    a. transform to lower case 
 	#    b. remove trailing punctuation
 
 	tokens = [re.sub('^\W+|\W$','',item.lower()) for item in forms]
 
-	# x. count the total number of types and tokens and calculate type-token ratio
+	# 8. count the total number of types and tokens and calculate type-token ratio
 
 	for token_pos, token in enumerate(tokens): 
 
@@ -45,35 +46,31 @@ for file_pos,file in enumerate(in_files):
 		except:
 			index[token] = [(file_pos,token_pos)]
 
-
+# 9. search for term
 span = 4
 search_term = "internet"
 search_hits = index[search_term]
 number_of_hits = len(search_hits)
 
+
+# 10. loop over search terms
 for hit_number,hit in enumerate(search_hits):
+
+	# 11. retrieve node position and form
 	file = hit[0]
 	node_pos = hit[1]
 	node_form = text_list[file][node_pos]
 
+	# 12. get left and right context of node
 	left = text_list[file][max(0,node_pos-span):node_pos]
 
 	right = text_list[file][node_pos+1:node_pos+1+span]
 
-
+	# 13. print KWIC line
 	print("%i\t%s\t%s\t%s\t%s" % (hit_number,' '.join(left),node_form,' '.join(right), in_files[file]))
 
 '''
-# 7. sort word list by frequency
-frequency_distribution = list(frequency_dist.items())
-frequency_distribution.sort(key=lambda x:x[1], reverse=True)
-
-
-# 8. write frequency list out to a file
-for item in frequency_distribution[0:20]:
-    print(item[0],"\t",item[1])
-
-out_directory = "../output/wordlists"    
+out_directory = "../output/kwic"    
 
 outfile = open(os.path.join(out_directory, 'articles_wordlist_freq.txt'),'w')
 for item in frequency_distribution:
